849e8967384ba180c7130c5de9ff1829d71d3e87
maxiaoqu <maxiaoqu@163.com>
2020-02-05 10:30:21 +0800

    增加多语言文件

--- src/index.js
+++ src/index.js
@@ -1,2 +1,42 @@
-import Button from './components/button'
 import Icon from './components/icon'
+
+const idoComponents = {
+  Icon
+};
+
+const install = function (Vue, opts = {}) {
+  if (install.installed) return;
+  locale.use(opts.locale);
+  locale.i18n(opts.i18n);
+
+  Object.keys(idoComponents).forEach(key => {
+    Vue.component(key, idoComponents[key]);
+  });
+
+  Vue.prototype.$IDO = {
+    size: opts.size || '',
+    transfer: 'transfer' in opts ? opts.transfer : '',
+    capture: 'capture' in opts ? opts.capture : true
+  };
+};
+
+// auto install
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
+const IDOAPI = {
+  version: process.env.VERSION, // eslint-disable-line no-undef
+  locale: locale.use,
+  i18n: locale.i18n,
+  install,
+  ...components
+};
+
+IDOAPI.lang = (code) => {
+  const langObject = window['ido/locale'].default;
+  if (code === langObject.i.locale) locale.use(langObject);
+  else console.log(`The ${code} language pack is not loaded.`); // eslint-disable-line no-console
+};
+
+module.exports.default = module.exports = IDOAPI;   // eslint-disable-line no-undef
