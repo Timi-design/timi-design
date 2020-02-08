09565ac9325a46c24d24c2e428adbf5ba98f0728
maxiaoqu <maxiaoqu@163.com>
2020-02-08 11:24:41 +0800

    修改配置文件

--- ido-tools/build-style.js
+++ ido-tools/build-style.js
@@ -1,25 +1,30 @@
-const gulp = require('gulp');
+'use strict';
+
+const {series, src, dest} = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const less = require('gulp-less');
 const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
 
 // 编译less
-gulp.task('css', function () {
-  gulp.src('../src/styles/index.less')
-    .pipe(less().a)
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions', 'ie > 8']
-    }))
+function compile() {
+  return src('../src/styles/index.less')
+    .pipe(less())
     .pipe(cleanCSS())
-    .pipe(rename('idoDesign.css'))
-    .pipe(gulp.dest('../dist/styles'));
-});
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'ie > 8'],
+      cascade: false
+    }))
+    .pipe(rename({
+      basename: 'idoDesign'
+    }))
+    .pipe(dest('../dist/styles'));
+}
 
 // 拷贝字体文件
-gulp.task('fonts', function () {
-  gulp.src('../src/styles/common/iconfont/fonts/*.*')
-    .pipe(gulp.dest('../dist/styles/fonts'));
-});
+function copyfont() {
+  return src('../src/styles/common/iconfont/fonts/*.*')
+    .pipe(dest('../dist/styles/fonts'));
+}
 
-gulp.task('default', ['css', 'fonts']);
+exports.build = series(compile, copyfont);
